6eeb4efbfedb2fb6702c0724d7af032f46ca4186
kabinet303 <kabinet303@localhost.localdomain>
2025-10-03 09:54:18 +0300

    структурка

---
+++ naru_net/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Нарушениям нет</title>
+    <link rel='icon' href='image/speed.png'>
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+<body>
+    <header> <img src='image/speed.png' alt='логотип'>
+        <h1>Нарушениям нет</h1>
+    </header>
+
+    <nav>
+        <a href="/demoexz/naru_net">Главная</a>
+        <a href="/demoexz/naru_net/admin">Админ-панель</a>
+    </nav>
+
+    <main>
+        <h1>Нарушениям нет</h1>
+        <footer>
+            2025
+        <footer>
+    </main>
+
+    <script src="js/script.js"></script>
+    
+
+</body>
+</html>
